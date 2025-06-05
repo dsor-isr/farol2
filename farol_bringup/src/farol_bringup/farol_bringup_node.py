210bbcaa608b5fe95a07df406e68c935f3b48a69
ecunhado <eduardo.m.a.cunha@tecnico.ulisboa.pt>
2025-06-05 12:45:39 +0100

    Fix(farol_bringup): fix node's on shutdown process

--- farol_bringup/src/farol_bringup/farol_bringup_node.py
+++ farol_bringup/src/farol_bringup/farol_bringup_node.py
@@ -396,14 +396,17 @@ def main(args=None):
 
   farol_bringup = FarolBringup()
 
-  rclpy.spin(farol_bringup)
-
-  # Destroy the node explicitly
-  # (optional - otherwise it will be done automatically
-  # when the garbage collector destroys the node object)
-  farol_bringup.cleanRosProcesses()
-  farol_bringup.destroy_node()
-  rclpy.shutdown()
+  try:
+    rclpy.spin(farol_bringup)
+  except KeyboardInterrupt:
+    pass
+  finally:
+    farol_bringup.cleanRosProcesses()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    farol_bringup.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
