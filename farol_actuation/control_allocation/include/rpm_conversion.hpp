5d18858e0b846d9355915d547937b495e08aae45
ecunhado <eduardo.m.a.cunha@tecnico.ulisboa.pt>
2025-10-28 19:08:01 +0000

    Update(control_allocation): add different mode of operation to rpm conversion

--- farol_actuation/control_allocation/include/rpm_conversion.hpp
+++ farol_actuation/control_allocation/include/rpm_conversion.hpp
@@ -9,6 +9,7 @@
 #include "std_msgs/msg/float32.hpp"
 #include "control_allocation/msg/thruster_force.hpp"
 #include "control_allocation/msg/thruster_rpm.hpp"
+#include "farol_msgs/msg/navigation_state.hpp"
 
 /**
  * @brief   RPM Conversion
@@ -34,27 +35,28 @@ class RPMConversion : public rclcpp::Node {
     /* Initialise Services */
     void initialiseServices();
 
-    /* Initialise Timers */
-    void initialiseTimers();
-    
-    /* Timer callback */
-    void timerCallback();
-
   private:
-    /* Timer for node's callbacks */
-    rclcpp::TimerBase::SharedPtr timer_;
 
     /* Declare publishers, subscribers, services, etc. */
     rclcpp::Subscription<control_allocation::msg::ThrusterForce>::SharedPtr thruster_force_sub_;
+    rclcpp::Subscription<farol_msgs::msg::NavigationState>::SharedPtr nav_state_sub_;
+
     rclcpp::Publisher<control_allocation::msg::ThrusterRPM>::SharedPtr rpm_command_pub_;
     
     /* Callbacks */
     void thrusterForceCallback(const control_allocation::msg::ThrusterForce &msg);
+    void navStateCallback(const farol_msgs::msg::NavigationState &msg);
 
     /* Other functions */
     
     /* Other variables */
     control_allocation::msg::ThrusterRPM rpm_command_msg_;
+    int mode_ = 1;
     std::vector<double> coef_fwd_, coef_bwd_;
     double rpm_value_, max_rpm_, min_rpm_;
+    double rho_;
+    double K_T_BP_;
+    double prop_pitch_;
+    double D_;
+    double surge_ = 0.0;
 };
