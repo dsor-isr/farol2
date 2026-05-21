649e2ade071d714b8c1c22d1fae3abda15a19469
Ravi Regalo <raviregalo@gmail.com>
2026-05-21 01:32:59 +0100

    Remove old filter architecture code

--- farol2_nav/launch/farol2_nav.launch.py
+++ farol2_nav/launch/farol2_nav.launch.py
@@ -41,13 +41,6 @@ def generate_launch_description():
     description='Path to the config package, usually the personal bringup of the workspace, in the src folder'
   )
 
-
-  launch_sample_and_hold_arg = DeclareLaunchArgument(
-    'sample_and_hold',
-    default_value='true',
-    description='Boolean to determine if "sample_and_hold" node is launched.'
-  )
-
   launch_filter_node_arg = DeclareLaunchArgument(
     'filter_node',
     default_value='false',
@@ -87,48 +80,8 @@ def generate_launch_description():
   ###################
   # Nodes to launch #
   ###################
-  
-  filter_handler_node = Node(
-    package='farol2_nav',
-    namespace=PathJoinSubstitution([LaunchConfiguration('vehicle_ns'), 'nav']),
-    executable='filter_handler',
-    name='filter_handler',
-    output='screen',
-    parameters=params,
-    remappings=[
-      # Publishers
-      ('state', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/state')]),
-      ('nav_sat_fix', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/nav_sat_fix')]),
-      # Services
-      ('change_filter', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/change_filter')]),
-    ]
-  )
 
-  sample_and_hold_filter_node = Node(
-    package='farol2_nav',
-    namespace=PathJoinSubstitution([LaunchConfiguration('vehicle_ns'), 'nav']),
-    executable='sample_and_hold',
-    name='sample_and_hold',
-    output='screen',
-    condition=IfCondition(LaunchConfiguration('sample_and_hold')),
-    parameters=params,
-    remappings=[
-      # Subscribers
-      ('measurement', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/measurement')]),
-      ('rpm_command', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/allocation/rpm_command')]),
-      ('rudder_command', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/allocation/rudder_command')]),
-      # Publishers
-      ('state', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/state')]),
-      ('position_raw', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/position_raw')]),
-      ('model_velocity', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/model_velocity')]),
-      ('course_meas_debug1', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/course_meas_debug1')]),
-      ('course_meas_debug2', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/course_meas_debug2')]),
-      # Services
-      ('tune_position_ekf', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/nav/filter/tune_position_ekf')]),
-    ]
-  )
-
-  new_filter_node = Node(
+  filter_node = Node(
     package='farol2_nav',
     namespace=PathJoinSubstitution([LaunchConfiguration('vehicle_ns'), 'nav']),
     executable='filter_node',
@@ -175,12 +128,11 @@ def generate_launch_description():
     use_sim_time_arg,
     config_package_path_share_arg,
     config_package_path_real_arg,
-    launch_sample_and_hold_arg,
     launch_filter_node_arg,
     # ...
     # nodes
     # filter_handler_node,
     # sample_and_hold_filter_node,
-    new_filter_node,
+    filter_node,
     # ... 
   ])
