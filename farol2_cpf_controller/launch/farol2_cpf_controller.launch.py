6c6df809addcb99e732059f23bc01e90d74b96b1
uuooffjjkkaa-new <gorka.monteiro@tecnico.ulisboa.pt>
2026-06-01 15:20:15 +0100

    CPF Launch - Corrected

--- farol2_cpf_controller/launch/farol2_cpf_controller.launch.py
+++ farol2_cpf_controller/launch/farol2_cpf_controller.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression, TextSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.conditions import IfCondition
@@ -38,34 +38,37 @@ def generate_launch_description():
       description='Run CPF node'
     )
 
+    vehicle_ns = PythonExpression(["'", LaunchConfiguration('vehicle_name'), "' + '", LaunchConfiguration('vehicle_id'), "'"])
+
     ###################################
     # Define parameters for all nodes #
     ###################################
     params = [
-        
-              {'vehicle_ns': LaunchConfiguration('vehicle_ns')},
-              {'use_sim_time': LaunchConfiguration('use_sim_time')},
+            {'vehicle_name': LaunchConfiguration('vehicle_name')},
+            {'vehicle_id': LaunchConfiguration('vehicle_id')},
+            {'use_sim_time': LaunchConfiguration('use_sim_time')},
 
-              PathJoinSubstitution([
-                  FindPackageShare('farol2_bringup'),
-                  'config_default',
-                  'vehicles',
-                  LaunchConfiguration('vehicle_name'),
-                  'cpf_controller.yaml'
-              ]),
+            PathJoinSubstitution([
+              FindPackageShare('farol2_bringup'),
+              'config',
+              LaunchConfiguration('vehicle_name'),
+              'default',
+              'cpf_controller.yaml'
+            ]),
+            # override config — same path as default when config_to_use == 'default'
+            PathJoinSubstitution([
+              FindPackageShare('farol2_bringup'),
+              'config',
+              LaunchConfiguration('vehicle_name'),
+              LaunchConfiguration('config_to_use'),
+              'cpf_controller.yaml'
+            ]),
+          ]
 
-              PathJoinSubstitution([
-                  LaunchConfiguration('config_package_path_share'),
-                  'config_personal',
-                  'vehicles',
-                  LaunchConfiguration('vehicle_name'),
-                  'cpf_controller.yaml'
-              ])
-    ]
 
     cpf_node = Node(
         package='farol2_cpf_controller',
-        namespace=PathJoinSubstitution([LaunchConfiguration('vehicle_ns'), 'control']),
+        namespace=PathJoinSubstitution([vehicle_ns, 'control']),
         executable='cpf_controller_node',
         name='cpf_controller',
         output='screen',
@@ -74,14 +77,14 @@ def generate_launch_description():
         remappings=[
             # topics used in the design/images -- adapt names if your node uses others
             ('external_gamma', [TextSubstitution(text='/cpf_broadcast/path_data')]),
-            ('internal_gamma', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/control/path_following/gamma')]),
+            ('internal_gamma', [TextSubstitution(text='/'), vehicle_ns, TextSubstitution(text='/control/path_following/gamma')]),
             # Publishers 
-            ('vc', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/control/path_following/vc')]),
+            ('vc', [TextSubstitution(text='/'), vehicle_ns, TextSubstitution(text='/control/path_following/vc')]),
             ('broadcast_data', [TextSubstitution(text='/cpf_broadcast/path_data')]),
             # services (ChangeTopology / StartStop)
-            ('change_topology', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/control/cpf_controller/change_topology')]),
-            ('start_cpf', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/control/cpf_controller/start_cpf')]),
-            ('stop_cpf', [TextSubstitution(text='/'), LaunchConfiguration('vehicle_ns'), TextSubstitution(text='/control/cpf_controller/stop_cpf')]),
+            ('change_topology', [TextSubstitution(text='/'), vehicle_ns, TextSubstitution(text='/control/cpf_controller/change_topology')]),
+            ('start_cpf', [TextSubstitution(text='/'), vehicle_ns, TextSubstitution(text='/control/cpf_controller/start_cpf')]),
+            ('stop_cpf', [TextSubstitution(text='/'), vehicle_ns, TextSubstitution(text='/control/cpf_controller/stop_cpf')]),
         ]
     )
 
