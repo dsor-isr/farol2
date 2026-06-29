8f802d80e0da3233a9ca361d67c4e1588e1f6790
Ravi Regalo <raviregalo@gmail.com>
2026-06-29 19:20:22 +0100

    Delete old farol_script package, now replaced with farol2_cli

--- farol2_scripts/display_git_branch_in_prompt.sh
+++
@@ -1,6 +0,0 @@
-_farol_prompt_profile="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../farol2_cli/profile.d/prompt_git_branch.bash"
-if [[ -f "$_farol_prompt_profile" ]]; then
-    # shellcheck source=/dev/null
-    source "$_farol_prompt_profile"
-fi
-unset _farol_prompt_profile
